1d5813a9ab6ce35e7549f71ed4aa562fa297278a
Roma <31734788+romaGri@users.noreply.github.com>
2019-03-25 23:34:30 +0300

    add md5 calculation

--- 08-AsyncIO/AsyncIO/Tasks.cs
+++ 08-AsyncIO/AsyncIO/Tasks.cs
@@ -5,6 +5,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using ServiceStack.Text;
@@ -20,24 +21,24 @@ namespace AsyncIO
         /// </summary>
         /// <param name="uris">Sequence of required uri</param>
         /// <returns>The sequence of downloaded url content</returns>
-        public static IEnumerable<string> GetUrlContent(this IEnumerable<Uri> uris) 
+        public static IEnumerable<string> GetUrlContent(this IEnumerable<Uri> uris)
         {
             // TODO : Implement GetUrlContent
-           
-            WebClient client = new WebClient { Encoding = Encoding.UTF8 };
 
-           // client.Headers[HttpRequestHeader.AcceptEncoding] = "gzip";
-            //var responseStream = new GZipStream(client.OpenRead(uris.ToString()), CompressionMode.Decompress);
-            //var reader = new StreamReader(responseStream);
+            WebClient client = new WebClient { Encoding = Encoding.UTF8 };
             IEnumerable<string> dnlad;
-            
-            //foreach (var r in reader)
+            //string d;
+            //foreach (var uri in uris)
             //{
-            //    dnlad = r;
+            //    client.Headers[HttpRequestHeader.AcceptEncoding] = "gzip";
+            //    var responseStream = new GZipStream(client.OpenRead(uri), CompressionMode.Decompress);
+            //    var reader = new StreamReader(responseStream);
+            //    var textResponse = reader.ReadToEnd();
+
             //}
+
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-           
-            dnlad = uris.Select(uri => client.DownloadString(uri.ToString()));
+            dnlad = uris.Select(uri => client.DownloadString(uri));
             return dnlad;
 
         }
@@ -98,10 +99,27 @@ namespace AsyncIO
         /// </summary>
         /// <param name="resource">Uri of resource</param>
         /// <returns>MD5 hash</returns>
-        public static Task<string> GetMD5Async(this Uri resource)
+        public static async Task<string> GetMD5Async(this Uri resource)
         {
             // TODO : Implement GetMD5Async
-            throw new NotImplementedException();
+            // var data = MD5.Create().ComputeHash(await new WebClient().DownloadDataAsync(resource)).Select<byte, string>(x => x.ToString());
+            //using (Stream stream = await new WebClient { Encoding = Encoding.UTF8 }.OpenReadTaskAsync(resource))
+            //{
+            //    var hashAlgoritm = MD5.Create();
+            //    var hashValue = hashAlgoritm.ComputeHash(stream);
+            //    return BitConverter.ToString(hashValue).Replace("-", string.Empty);
+            //}
+            var data = MD5.Create().ComputeHash(await new WebClient().DownloadDataTaskAsync(resource))
+            .Select<byte, string>(x => x.ToString()).ToArray();
+            return string.Concat(data);
+
+            //using (Stream stream = await new WebClient { Encoding = Encoding.UTF8 }.OpenReadTaskAsync(resource))
+            //{
+            //    var hashAlgorithm = MD5.Create();
+            //    var hashValue = hashAlgorithm.ComputeHash(stream);
+            //    return BitConverter.ToString(hashValue).Replace("-", string.Empty);
+            //}
+
         }
 
     }
